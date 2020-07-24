2de2071d80d9418c34309172daa7dd6b5236a2ed
de24world <de24world@gmail.com>
2020-07-24 09:34:15 +0200

    reference add

--- index.js
+++ index.js
@@ -61,3 +61,61 @@ class App extends Component {
 }
 
 render(<App />, document.getElementById('root'));
+
+
+/*
+
+
+
+https://github.com/szdc/tiktok-api
+https://stackoverflow.com/questions/44109326/how-to-work-with-json-in-react-js
+https://pusher.com/tutorials/consume-restful-api-react
+https://scotch.io/courses/10-react-challenges-beginner/fetch-and-display-from-an-api
+https://www.youtube.com/watch?v=c6t3bW7kx6E
+
+
+function MyComponent() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.items);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.name}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+
+*/
